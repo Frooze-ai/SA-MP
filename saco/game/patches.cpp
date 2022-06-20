85618c97b705b8ada0ed5a1978c00b710ce05ddc
RD42 <42702181+dashr9230@users.noreply.github.com>
2022-06-20 12:25:33 +0200

    Add more game patches to ApplyPreGamePatches

--- saco/game/patches.cpp
+++ saco/game/patches.cpp
@@ -18,6 +18,9 @@ extern CNetGame* pNetGame;
 extern CGame* pGame;
 extern DWORD dwUIMode;
 
+DWORD dwSystemMemory;
+DWORD dwStreamingMemory;
+
 //----------------------------------------------------------
 
 #define memadd(num,arr) dwAlloc+=num;cBytes=(char*)&dwAlloc;for(int i=0;i<4;i++)arr[i]=cBytes[i]
@@ -179,12 +182,63 @@ BOOL ApplyPreGamePatches()
 	strcpy((PCHAR)0x866CD8,"title");
 	strcpy((PCHAR)0x866CCC,"title");
 
+	// allow more resolutions by removing aspect ratio conditions
+	UnFuck(0x745B87,68);
+	memset((PVOID)0x745B87,0x90,68);
+
+	// allow widescreen resolutions
+	UnFuck(0x7459E1,2);
+	memset((PVOID)0x7459E1,0x90,2);
+
+	// change timer to rely on RenderWare's RsTimer()?
+	UnFuckAndCheck(0x561872,30,0x85);
+	*(PBYTE)0x561872 = 0x33;
+	*(PBYTE)0x561873 = 0xC0;
+	memset((PVOID)0x561874,0x90,27);
+
+	MEMORYSTATUSEX statex;
+	statex.dwLength = sizeof(statex);
+	GlobalMemoryStatusEx(&statex);
+	
+	dwSystemMemory = statex.ullTotalPhys / (1024 * 1024);
+
+	if(dwSystemMemory > 4000)
+		dwStreamingMemory = 0x40000000; // 1024MB
+	else if(dwSystemMemory > 2000)
+		dwStreamingMemory = 0x20000000; // 512MB
+	else if(dwSystemMemory > 1000)
+		dwStreamingMemory = 0x10000000; // 256MB
+	else if(dwSystemMemory > 500)
+		dwStreamingMemory = 0x08000000; // 128MB
+	else
+		dwStreamingMemory = 0x06000000; // 96MB
+
 	// Modify the streaming memory hardcoded values
 	UnFuck(0x5B8E6A,4);
-	*(DWORD *)0x5B8E6A = 134217728; // 128MB
+	//*(DWORD *)0x5B8E6A = 134217728; // 128MB
+	*(DWORD *)0x5B8E6A = dwStreamingMemory;
 
 	// For SCM disable/enable
+	// TODO: Comment this out. It's not present in the latest version
+	// but this is required to get in the game
 	UnFuck(0x469EF5,2);
+
+	// prevent processing image files (GTA3.IMG, GTA_INT.IMG)
+	// manually load these in default.dat
+	UnFuckAndCheck(0x4083C0,1,0xB8);
+	*(PBYTE)0x4083C0 = 0xC3;
+
+	// Disable transitions/fading on loading screen
+	UnFuck(0x590099,5);
+	memset((PVOID)0x590099,0x90,5);
+
+	// Frame sleep
+	UnFuck(0x53E94C,1);
+	*(PBYTE)0x53E94C = 2;
+
+	// Increase TexDictionary
+	UnFuck(0x731F60,4);
+	*(DWORD*)0x731F60 = 20000;
 
 	return TRUE;
 }
